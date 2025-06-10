11e09c125929cdcf6b54a56bf07dade2a13fa00c
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2025-06-10 22:42:18 +0200

    Fix it?

--- src/cpu/pred/simple_btb.cc
+++ src/cpu/pred/simple_btb.cc
@@ -48,8 +48,8 @@
 #include "debug/BTBModeClear.hh"
 #include "debug/BTBModeClearReverse.hh"
 #include "debug/BTBModePriority.hh"
-#include "debug/BTBModePriorityU.hh"
-#include "debug/BTBModePriorityK.hh"
+#include "debug/BMPU.hh"
+#include "debug/BMPK.hh"
 
 namespace gem5::branch_prediction
 {
@@ -162,7 +162,7 @@ SimpleBTB::update(ThreadID tid, Addr instPC,
     BTBEntry *victim = nullptr;
     bool wasValid = false;
 
-    if (::gem5::debug::BTBModePriority || (::gem5::debug::BTBModePriorityU && !isKernelMode ) || (::gem5::debug::BTBModePriorityK && isKernelMode)) {
+    if (::gem5::debug::BTBModePriority || (::gem5::debug::BMPU && !isKernelMode ) || (::gem5::debug::BMPK && isKernelMode)) {
         for (auto &entry : btb) {
             if (entry.isKernelMode != isKernelMode) {
                 if (
